31e2d811eeedf22bd991aba9775762507af62a1d
Rahul Kumar <rahulkumar9199270356@gmail.com>
2023-10-30 09:41:38 +0530

    init

---
+++ checkthenum.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num=18;
+    if(num>0){
+        cout << "positive"<< endl;
+    }
+    else if(num<0){
+        cout << "negative"<< endl;
+    }
+    else {
+        cout << "zero"<<endl;
+    }
+}
